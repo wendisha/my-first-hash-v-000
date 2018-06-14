ca06e4506e924271d1fdfb52298de49ad3ec7274
Wendy Calderon <wendycalderon@gmail.com>
2018-06-14 03:36:41 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
   your_hash = {"tv_show" => "Game Of Thrones", "movie" => "Interstellar"}
-  
 end
 
 
@@ -10,8 +9,8 @@ def shipping_manifest
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
   the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-
 end
+
 
 def retrieval
   shipping_manifest = {
@@ -19,7 +18,6 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
   shipping_manifest ["oil paintings"]
 end
