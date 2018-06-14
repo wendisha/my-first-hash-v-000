c69ce6b3feb3f619726ae2d4f4e6b3bdbd687391
Wendy Calderon <wendycalderon@gmail.com>
2018-06-14 03:33:25 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -21,8 +21,9 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+  shipping_manifest ["oil paintings"]
 end
+
 
 def adding
   shipping_manifest = {
@@ -31,13 +32,13 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
+  shipping_manifest ["muskets"] = 2
 
 
   # add 4 gun powder to the shipping_manifest hash below
-
+  shipping_manifest ["gun powder"] = 4
 
 
   # return the shipping_manifest hash below
-
+  shipping_manifest
 end
